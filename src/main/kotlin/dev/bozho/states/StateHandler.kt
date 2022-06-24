fc78fc8f30e8bc7c70a1d6c08a09d729562b737c
mi0epro <smokemi0123@gmail.com>
2022-06-24 20:03:06 +0300

    added StateHandler

---
+++ src/main/kotlin/dev/bozho/states/StateHandler.kt
@@ -0,0 +1,39 @@
+package dev.bozho.states
+
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import net.minecraftforge.fml.common.gameevent.TickEvent
+import net.minikloon.fsmgasm.StateSeries
+
+object StateHandler {
+    private var state: StateSeries = StateSeries()
+
+    fun scheduleRotateState() {
+        state = StateSeries(
+            WaitState(20),
+            //RotationState(mc.thePlayer.rotationYaw + 180F, mc.thePlayer.rotationPitch, 25)
+        )
+    }
+
+    fun startState() {
+        state.start()
+    }
+
+    fun freezeState() {
+        state.frozen = true
+    }
+
+    fun unfreezeState() {
+        state.frozen = false
+    }
+
+    fun stopState() {
+        state.end()
+    }
+
+    @SubscribeEvent
+    fun onTick(event: TickEvent.ClientTickEvent) {
+        if (state.started && !state.ended) {
+            state.update()
+        }
+    }
+}
